ccf957d498c6339d97ac2815d5f5d1c11abfc75b
juanb <mail2juanb@yahoo.fr>
2025-03-08 00:13:58 +0100

    Refactor : RuleName domain ; cleanup getter and setter

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -1,15 +1,10 @@
 package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 
-//@Getter
-//@Setter
-//@AllArgsConstructor
+
 @Data
 @Entity
 @Table(name = "rulename")
@@ -48,73 +43,4 @@ public class RuleName implements DomainModel<RuleName> {
         return this;
     }
 
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getJson() {
-        return json;
-    }
-
-    public void setJson(String json) {
-        this.json = json;
-    }
-
-    public String getTemplate() {
-        return template;
-    }
-
-    public void setTemplate(String template) {
-        this.template = template;
-    }
-
-    public String getSqlStr() {
-        return sqlStr;
-    }
-
-    public void setSqlStr(String sqlStr) {
-        this.sqlStr = sqlStr;
-    }
-
-    public String getSqlPart() {
-        return sqlPart;
-    }
-
-    public void setSqlPart(String sqlPart) {
-        this.sqlPart = sqlPart;
-    }
-
-    @Override
-    public String toString() {
-        return "RuleName{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", json='" + json + '\'' +
-                ", template='" + template + '\'' +
-                ", sqlStr='" + sqlStr + '\'' +
-                ", sqlPart='" + sqlPart + '\'' +
-                '}';
-    }
 }
